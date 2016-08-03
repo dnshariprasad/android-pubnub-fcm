0e50bb3484712b96d69d2bbabb3d736ae6f77c08
Hari Prasad <hari.kvana@gmail.com>
2016-08-03 22:09:19 +0530

    basic flow added for pubnub

--- app/src/main/java/com/pubnubchat/Constant.java
+++ app/src/main/java/com/pubnubchat/Constant.java
@@ -5,6 +5,7 @@ package com.pubnubchat;
  */
 public class Constant {
     public static final class pubnub {
+        public static final String CHANNEL = "my_channel";
         public static final String PUBLISH_KEY = "pub-c-5ee6c9c0-dc43-4798-b149-1ce29b770be9";
         public static final String SUBSCRIBE_KEY = "sub-c-0bb6ff8c-596b-11e6-8c8a-02ee2ddab7fe";
         public static final String SECRET_KEY = "sec-c-OTkwZmYxODUtMzgxYS00ZDZlLWE0MjAtZGMyZTI1NzY3NTM4";
